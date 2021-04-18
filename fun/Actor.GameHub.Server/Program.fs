1a801c7019bb6a9f5eeebf49d5dc8387fed2ef4a
Lars Hanisch <dvb@flensrocker.de>
2021-04-18 14:25:42 +0200

    F# - Server starts the system

--- fun/Actor.GameHub.Server/Program.fs
+++ fun/Actor.GameHub.Server/Program.fs
@@ -1,3 +1,29 @@
+open System
+open System.IO
+open Akka.FSharp
+
+let getConfig filename =
+    match File.Exists filename with
+    | false -> Configuration.defaultConfig ()
+    | true ->
+        filename
+        |> File.ReadAllText
+        |> Configuration.parse
+
 [<EntryPoint>]
 let main argv =
+    let configFilename =
+        match Array.toList argv with
+        | arg0 :: _ -> arg0
+        | _ -> "gamehub-seed.akka"
+
+    use gameHubServerSystem =
+        configFilename |> getConfig |> System.create "GameHub"
+
+    Console.ReadLine() |> ignore
+
+    gameHubServerSystem.Terminate()
+    |> Async.AwaitTask
+    |> Async.RunSynchronously
+
     0
